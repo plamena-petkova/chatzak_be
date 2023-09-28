8e62c6ddef7b903f300c650d5ae410ca7530f0fc
Plamena Petkova <pl.petkova@gmail.com>
2023-09-28 16:08:46 +0300

    Login and getAllUsers added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,3 +33,39 @@ module.exports.register = async (req, res, next) => {
     
 
 }
+
+module.exports.login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({username});
+    
+        if(!user) {
+            return res.json({msg: "Incorrect username or password", status: false});
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+        if(!isPasswordValid) {
+            return res.json({msg: "Incorrect username or password", status: false});
+        }
+
+        delete user.password;
+
+
+        return res.json({status: true, user});
+
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        res.json({status: true, users});
+    } catch(error) {
+        console.error('Something went wrong!', error);
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
-const { register } = require("../controllers/usersController");
+const { register, login, getAllUsers } = require("../controllers/usersController");
 
 
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/all-users", getAllUsers);
 
 module.exports = router;
  
