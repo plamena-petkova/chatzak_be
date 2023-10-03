4ade25773f7cb659ca017aa86af4266e08ccd0ac
Plamena Petkova <pl.petkova@gmail.com>
2023-10-03 20:08:38 +0300

    Implement socket.io

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const userRoute = require('../chatzak_be/routes/userRoutes')
-const messagesRoute = require('../chatzak_be/routes/messagesRoutes')
-
+const userRoute = require('../chatzak_be/routes/userRoutes');
+const messagesRoute = require('../chatzak_be/routes/messagesRoutes');
+const socket = require("socket.io");
 const app = express();
 require('dotenv').config();
+
 
 app.use(cors());
 app.use(express.json());
@@ -24,4 +25,28 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port:${process.env.PORT}`)
-}) 
+});
+
+const io = socket(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        credentials: true,
+    }
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId , socket.id)
+    });
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-receive", data.message);
+        }
+    });
+});
+
