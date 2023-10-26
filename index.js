9beb79770a7f5c1fcfdf394cca82349d4f1792ff
Plamena Petkova <pl.petkova@gmail.com>
2023-10-26 22:24:23 +0300

    Send online user to fe to show badge

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRoute = require('../chatzak_be/routes/userRoutes');
-const messagesRoute = require('../chatzak_be/routes/messagesRoutes');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("../chatzak_be/routes/userRoutes");
+const messagesRoute = require("../chatzak_be/routes/messagesRoutes");
 const socket = require("socket.io");
 const app = express();
-require('dotenv').config();
-
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', userRoute);
-app.use('/api/messages', messagesRoute);
+app.use("/api/auth", userRoute);
+app.use("/api/messages", messagesRoute);
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log('DB connection succesfully!')
-}).catch((e) => {
-    console.log('Error', e);
-});
+  })
+  .then(() => {
+    console.log("DB connection succesfully!");
+  })
+  .catch((e) => {
+    console.log("Error", e);
+  });
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port:${process.env.PORT}`)
+  console.log(`Server is running on port:${process.env.PORT}`);
 });
 
 const io = socket(server, {
-    cors:{
-        origin:"http://localhost:3000",
-        credentials: true,
-    }
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
 });
 
 global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-        onlineUsers.set(userId , socket.id)
-    });
+  global.chatSocket = socket;
+  socket.on("add-user", (newUserId) => {
+    onlineUsers.set(newUserId, socket.id);
+    const users = Array.from(onlineUsers).map(([userId, socketId]) => {
+        return { userId, socketId };
+      });
+    io.emit("update-users", users);
+  });
 
-    socket.on("send-msg", (data) => {
-        const sendUserSocket = onlineUsers.get(data.to);
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-receive", data.message);
-        }
-    });
-    socket.on("edit-msg", (data) => {
-        const sendUserSocket = onlineUsers.get(data.to);
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-edited", data.message);
-        }
-    });
+  socket.on("disconnect", () => {
+    const users = Array.from(onlineUsers).map(([userId, socketId]) => {
+        return { userId, socketId };
+      });
+    const usersOnline = users.filter((user) => user.socketId !== socket.id)
+    io.emit('update-users', usersOnline);
+  });
 
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+  socket.on("edit-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-edited", data.message);
+    }
+  });
 });
+
+
 
