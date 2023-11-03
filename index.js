26753902957de1d989fc60b8100dddf9394f9be3
Plamena Petkova <pl.petkova@gmail.com>
2023-11-03 14:34:20 +0200

    Added package

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoutes");
 const messagesRoute = require("./routes/messagesRoutes");
 const socket = require("socket.io");
+
 const compression = require("compression");
 const app = express();
 require("dotenv").config();
+
 
 app.use(compression());
 
