5338043d6266fe72291a7f47ad389280a59d5fea
Plamena Petkova <pl.petkova@gmail.com>
2023-11-03 23:53:18 +0200

    Change file path

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use("/api/messages", messagesRoute);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
