193a7cc305c57d6983b495fcf3c83d276fdba842
Plamena Petkova <pl.petkova@gmail.com>
2023-11-04 19:13:58 +0200

    Modified index.js added Browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,22 @@ import LoginView from "./views/LoginView";
 import "./App.css";
 import React from "react";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import RegisterView from "./views/RegisterView";
 import PrivateRoutes from "./components/PrivateRoutes";
 import HomeView from "./views/HomeView";
 
 function App() {
   return (
-      <BrowserRouter basename="/">
+      
         <Routes>
           <Route path="/" element={<HomeView />} />
-         
           <Route path="/login" element={<LoginView />} />
           <Route path="/register" element={<RegisterView />} />
           <Route element={<PrivateRoutes />}>
             <Route path="/chat" element={<ChatView />} />
           </Route>
         </Routes>
-      </BrowserRouter>
   );
 }
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,16 @@ import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>
 );
 
