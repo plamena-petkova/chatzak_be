0754624b1dc9f8e1db56b3750e7300d145940438
Plamena Petkova <pl.petkova@gmail.com>
2023-11-04 19:38:29 +0200

    Change basepath name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,23 @@ import LoginView from "./views/LoginView";
 import "./App.css";
 import React from "react";
 
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterView from "./views/RegisterView";
 import PrivateRoutes from "./components/PrivateRoutes";
 import HomeView from "./views/HomeView";
 
 function App() {
   return (
-      
+    <BrowserRouter basename="/api">
         <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/register" element={<RegisterView />} />
+          <Route path="/api" element={<HomeView />} />
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
           <Route element={<PrivateRoutes />}>
             <Route path="/chat" element={<ChatView />} />
           </Route>
         </Routes>
+      </BrowserRouter>
   );
 }
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,13 @@ import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
