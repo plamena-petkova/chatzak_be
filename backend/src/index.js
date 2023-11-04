17fe40c0ad51ea72e5c2a1af2a45049455ea3d18
Plamena Petkova <pl.petkova@gmail.com>
2023-11-04 22:54:56 +0200

    Resolve typo

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
